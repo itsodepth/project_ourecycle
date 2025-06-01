0cb1082ccfda5c084ecbae267a25868f6d071223
Muh Devano Alfarizy <devanoalfarizy04@gmail.com>
2025-06-01 21:17:36 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_ourecycle/pages/animated_navbar.dart';
+import 'package:project_ourecycle/pages/home_screen.dart';
+import 'package:project_ourecycle/pages/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+      home: const AnimationBar(),
+    );
+  }
+}
---
+++ lib/pages/animated_navbar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:project_ourecycle/pages/transaksi_screen.dart';
+import 'package:project_ourecycle/pages/setting_screen.dart';
+import 'package:project_ourecycle/pages/home_screen.dart';
+
+class AnimationBar extends StatefulWidget {
+  const AnimationBar({super.key});
+
+  @override
+  State<AnimationBar> createState() => _AnimationBarState();
+}
+
+class _AnimationBarState extends State<AnimationBar> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomeScreen(),
+    TransaksiScreen(),
+    SettingScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: Color(0xFF00AA13),
+        unselectedItemColor: Colors.grey,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w500),
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: 'Transaksi',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+      ),
+    );
+  }
+}
